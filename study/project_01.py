1348493f2279efbe35838dc794e5d094b6db1fd5
gow1231 <gow1231@gmail.net>
2017-06-02 00:31:16 +0900

    크롤링 코드으으으

--- study/project_01.py
+++ study/project_01.py
@@ -2,13 +2,13 @@ import urllib.request # 웹브라우저에서 html 문서를 얻어오기 위해
 from  bs4 import BeautifulSoup #html 문서 검색 모듈
 import os
 
-def get_save_path():
-    save_path = input("Enter the file name and file location :" )
-    save_path = save_path.replace("\\", "/")
-    if not os.path.isdir(os.path.split(save_path)[0]):
-        os.mkdir(os.path.split(save_path)[0])  # 폴더가 없으면 만드는 작업
-
-    return save_path
+# def get_save_path():
+#     save_path = input("C:\data_haeri" )
+#     save_path = save_path.replace("\\", "/")
+#     if not os.path.isdir(os.path.split(save_path)[0]):
+#         os.mkdir(os.path.split(save_path)[0])  # 폴더가 없으면 만드는 작업
+#
+#     return save_path
 
 def fetch_list_url():
 
@@ -16,9 +16,7 @@ def fetch_list_url():
 
         for num in range(0, 20):
 
-            list_url = "http://search.joins.com/JoongangNews?" \
-                      "page={}&Keyword=%EC%9D%B8%EA%B3%B5%EC%A7%80%EB%8A%A5&" \
-                      "SortType=New&SearchCategoryType=JoongangNews".format(num)
+            list_url = "http://search.joins.com/TotalNews?page="+str(num)+"&Keyword=%EB%AC%B8%EC%9E%AC%EC%9D%B8&SortType=New&SearchCategoryType=TotalNews"
             url = urllib.request.Request(list_url) # url 요청에 따른 http 통신 헤더값을 얻어낸다
             res = urllib.request.urlopen(url).read().decode("utf-8")
 
@@ -26,33 +24,36 @@ def fetch_list_url():
 
             for i in range(0,20):
                 try:
-                    soup2 = soup.find_all('div', class_="text")[i]
+                    soup2 = soup.find_all("div", class_="text")[i]
                     soup3 = soup2.find("a")["href"]
                     params.append(soup3)
                 except Exception:
                     pass
 
         return params
+        #print(params) 링크 타고 들어가니 기사..... 그냥 기사까지 전부 크롤링해도 괜찮겠는데?
+        # ['http://news.joins.com/article/21631035',
+        #  'http://news.joins.com/article/21631017', ...... ]
 
 fetch_list_url()
 
 def fetch_list_url2():
 
     params2 = fetch_list_url()
-    f = open(get_save_path(), 'w', encoding="utf-8") # w : write
-    #print(params2) #['http://www.hani.co.kr/arti/culture/book/778730.html', 'http://www.hani.co.kr/arti/science/science_general/778615.html', 'http://www.hani.co.kr/arti/economy/it/778234.html',......]
+    #f = open(get_save_path(), 'w', encoding="utf-8") # w : write
     for i in params2:
         list_url2 = i
         url2 = urllib.request.Request(list_url2)
         res = urllib.request.urlopen(url2).read().decode("utf-8")
         soup = BeautifulSoup(res, "html.parser")
-        result2 = soup.find_all('div', id="article_body")[0] # 굳이 for 문을 써서 tag를 바꾸지 않고 이렇게 바꿔도 됨!!!
+        result = soup.find_all('h1', id="article_title")[0] # 굳이 for 문을 써서 tag를 바꾸지 않고 이렇게 바꿔도 됨!!!
+        print(result.get_text(strip=True))
         #print(type(result2)) #<class 'bs4.element.ResultSet'>
 
-        result = soup.find_all('div',id="article_body")[0]
-        final = result.get_text(strip=True, separator='\n')
+        #result = soup.find_all('div',id="article_body")[0]
+        #final = result.get_text(strip=True, separator='\n')
 
-        f.write(final)
-    f.close()
+        #f.write(final)
+    #f.close()
 
 fetch_list_url2()
